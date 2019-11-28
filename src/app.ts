b8e37273f20d4030d6f7e30b73af17e3c216f26e
James Lee <uptownhr@gmail.com>
2019-11-27 21:43:40 -0800

    Created TwitterTokenSchema and Mongodb Connection Tokens are also being stored

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose'
+import {config} from 'dotenv'
+config()
+
+import express from 'express'
+import expressSession from 'express-session'
+import passport from 'passport'
+import {Strategy as TwitterStrategy} from 'passport-twitter'
+
+import TwitterTokens from './models/TwitterTokens'
+
+mongoose.connect(process.env.DB_CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const twitterStrategy = new TwitterStrategy({
+  consumerKey: process.env.TWITTER_KEY,
+  consumerSecret: process.env.TWITTER_SECRET,
+  callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+}, async (token, tokenSecret, profile, cb) => {
+  const twitterToken = await TwitterTokens.create({
+    token,
+    tokenSecret,
+    profile
+  })
+
+  cb(twitterToken)
+})
+
+
+
+passport.use(twitterStrategy)
+
+const app = express()
+
+const session = expressSession({
+  secret: 'seeexret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false
+  }
+})
+
+app.use(session)
+app.get('/oauth/twitter', passport.authenticate('twitter'));
+app.get('/oauth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    console.log('success')
+    res.redirect('/');
+  });
+
+app.use(passport.initialize())
+
+
+app.listen(3000)
---
+++ src/models/TwitterTokens.ts
@@ -0,0 +1,24 @@
+import {Schema, model} from 'mongoose'
+
+const {Mixed, String} = Schema.Types
+
+const TwitterProfilePhotoSchema = new Schema({
+  value: String
+})
+
+const TwitterProfileSchema = new Schema({
+  id: String,
+  username: String,
+  displayName: String,
+  photos: [TwitterProfilePhotoSchema],
+  _raw: String,
+  _json: Mixed
+})
+
+const TwitterTokensSchema = new Schema({
+  token: String,
+  tokenSecret: String,
+  profile: TwitterProfileSchema
+})
+
+export default model('TwitterTokens', TwitterTokensSchema)
